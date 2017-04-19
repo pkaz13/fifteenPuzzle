b85e71710fdbf306b8a0fc5e5972c8b49cf7b626
Piotr Kaźmierczak <195611@edu.p.lodz.pl>
2017-04-19 17:24:10 +0200

    Initial working

---
+++ FifteenPuzzle/FifteenPuzzle/FileHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FifteenPuzzle
+{
+    public class FileHelper
+    {
+        public string EntryFilePath { get; set; }
+        public string SolutionFilePath { get; set; }
+        public string StatsFilePath { get; set; }
+
+        public FileHelper(string entryFilePath)
+        {
+            EntryFilePath = entryFilePath;
+        }
+
+        public void ReadFromFile()
+        {
+
+        }
+    }
+}
---
+++ FifteenPuzzle/FifteenPuzzle/GameBoard.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FifteenPuzzle
+{
+    public class GameBoard
+    {
+        public int Columns { get; set; }
+
+        public int Rows { get; set; }
+
+        public int[,] Puzzles { get; set; }
+
+        public GameBoard(int rows, int columns)
+        {
+            this.Rows = rows;
+            this.Columns = columns;
+            this.Puzzles = new int[rows, columns];
+        }
+    }
+}
