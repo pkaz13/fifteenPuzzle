c5f8e637d6f1963feeceef3fe167fd0cecf50b5a
Piotr Kaźmierczak <pkazmierczak13@gmail.com>
2017-04-20 22:58:17 +0200

    Creating board, init with data from file

--- FifteenPuzzle/FifteenPuzzle/FileHelper.cs
+++ FifteenPuzzle/FifteenPuzzle/FileHelper.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,14 +13,36 @@ namespace FifteenPuzzle
         public string SolutionFilePath { get; set; }
         public string StatsFilePath { get; set; }
 
+        public string[] Lines { get; set; }
+
+        private int[] rowsAndColumns;
+        private int[,] data;
+
         public FileHelper(string entryFilePath)
         {
             EntryFilePath = entryFilePath;
+            Lines = File.ReadAllLines(EntryFilePath);
         }
 
-        public void ReadFromFile()
+        public int[] GetRowsAndColumns()
         {
+            rowsAndColumns = Lines[0].Split(new Char[] { ',', ' ' }).Select(int.Parse).ToArray();
+            return rowsAndColumns;
+        }
 
+        public int[,] GetData()
+        {
+            data = new int[rowsAndColumns[0], rowsAndColumns[1]];
+            for (int i = 0; i < Lines.Length - 1; i++)
+            {
+                int[] temp = Lines[i+1].Split(new Char[] { ',', ' ' }).Select(int.Parse).ToArray();
+                for (int j = 0; j < temp.Length; j++)
+                {
+                    data[i, j] = temp[j];
+                }
+            }
+            return data;
         }
     }
 }
+
--- FifteenPuzzle/FifteenPuzzle/Program.cs
+++ FifteenPuzzle/FifteenPuzzle/Program.cs
@@ -10,6 +10,26 @@ namespace FifteenPuzzle
     {
         static void Main(string[] args)
         {
+            string filePath = @"../../../4x4_01_00001.txt";
+
+            FileHelper helper = new FileHelper(filePath);
+
+            int[] temp = helper.GetRowsAndColumns();
+
+            GameBoard board = new GameBoard(temp[0], temp[1]);
+            board.Puzzles = helper.GetData();
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Console.Write(string.Format("{0} ", board.Puzzles[i, j]));
+                }
+                Console.Write(Environment.NewLine + Environment.NewLine);
+            }
+
+            Console.ReadKey();
+
         }
     }
 }
